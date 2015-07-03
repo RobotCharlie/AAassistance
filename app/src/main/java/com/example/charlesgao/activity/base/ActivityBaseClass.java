456034ae98ea895e757d90cb084d1d6fe0c83c8e
Charlie Gao <charles.gao.92@gmail.com>
2015-07-02 23:49:39 -0400

    header&footer

---
+++ app/src/main/java/com/example/charlesgao/activity/base/ActivityBaseClass.java
@@ -0,0 +1,23 @@
+package com.example.charlesgao.activity.base;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+
+/**
+ *Created by CharlesGao on 15-07-01.
+ * Function: This is the Class that encapsulate Non-business-Logic-related methods, in order to
+ *           make convenience to the regular methods-call like
+ *           {@link android.widget.Toast};
+ *           {@link android.content.Intent};
+ *           {@link android.app.AlertDialog}
+ */
+public class ActivityBaseClass extends Activity {
+
+    protected void ShowMsg(String string){
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
+    }
+
+
+
+}
