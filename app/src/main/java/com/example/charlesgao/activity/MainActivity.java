a5bac76bf47f5e738498c4f4fa4a6f7bee160d09
Charlie Gao <charles.gao.92@gmail.com>
2015-07-05 13:51:14 -0400

    finish the first step of grid view

--- app/src/main/java/com/example/charlesgao/activity/MainActivity.java
+++ app/src/main/java/com/example/charlesgao/activity/MainActivity.java
@@ -4,38 +4,44 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.GridView;
+
+import com.example.charlesgao.activity.adapter.AdapterAppGrid;
 import com.example.charlesgao.activity.base.ActivityFrameClass;
 
 
 public class MainActivity extends ActivityFrameClass {
 
+    private GridView mGridview;
+    private AdapterAppGrid mAdapterAppGrid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        AppendMainBody(R.layout.main_body);
+//        setContentView(R.layout.activity_main);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        InitVariable();
+        InitListener();
+        InitView();
+        BindData();
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void InitVariable(){
+        mAdapterAppGrid = new AdapterAppGrid(this);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void InitView(){
+        mGridview = (GridView)findViewById(R.id.id_main_body_grid_view);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    public void InitListener(){
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    public void BindData(){
+        mGridview.setAdapter(mAdapterAppGrid);
     }
 }
--- app/src/main/java/com/example/charlesgao/activity/adapter/AdapterAppGrid.java
+++ app/src/main/java/com/example/charlesgao/activity/adapter/AdapterAppGrid.java
@@ -1,11 +1,14 @@
 package com.example.charlesgao.activity.adapter;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.charlesgao.activity.R;
 
 /**
  * Created by CharlesGao on 15-07-03.
@@ -18,32 +21,62 @@ public class AdapterAppGrid extends BaseAdapter {
         TextView textView;
     }
 
-    private Integer[] ImageInteger = {
+    private Context mContext;
 
+    private Integer[] mImageInteger ={
+            R.drawable.app_grid_view_add_payment,
+            R.drawable.app_grid_view_check_payment,
+            R.drawable.app_grid_view_statistic,
+            R.drawable.app_grid_view_book_management,
+            R.drawable.app_grid_view_category_management,
+            R.drawable.app_grid_view_user_management
     };
 
+    private String[] mImageString = new String[6];
 
+    public AdapterAppGrid(Context context){
 
+        mContext = context;
+        mImageString[0] = context.getString(R.string.app_grid_view_add_payment);
+        mImageString[1] = context.getString(R.string.app_grid_view_check_payment);
+        mImageString[2] = context.getString(R.string.app_grid_view_statistic);
+        mImageString[3] = context.getString(R.string.app_grid_view_book_management);
+        mImageString[4] = context.getString(R.string.app_grid_view_category_management);
+        mImageString[5] = context.getString(R.string.app_grid_view_user_management);
 
-
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return mImageString.length;
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mImageString[position];
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        Holder holder;
+        if(convertView == null){
+            LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.main_body_item,null);
+            holder = new Holder();
+            holder.imageView = (ImageView)convertView.findViewById(R.id.id_iv_icon);
+            holder.textView = (TextView)convertView.findViewById(R.id.id_tv_name);
+            convertView.setTag(holder);
+        }else {
+            holder = (Holder)convertView.getTag();
+        }
+        holder.imageView.setImageResource(mImageInteger[position]);
+        holder.textView.setText(mImageString[position]);
+        return convertView;
     }
 }
